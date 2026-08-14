9d552018b3a6a8b4bbdfeeaef37bfea070a59dad
anonymous926160 <anonymous926160@gmail.com>
2026-08-14 07:18:20 -0500

    Update server.js

--- server.js
+++ server.js
@@ -13,6 +13,8 @@ import telegramRoutes from "./routes/telegram.js";
 
 const app = express();
 
+const PORT = process.env.PORT || 3000;
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
@@ -42,6 +44,6 @@ app.get("/:name", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000 🚀");
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT} 🚀`);
 });
